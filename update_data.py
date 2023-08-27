7ea7ec0b24d8a4a5f100aac3e20c0523173bc5a8
Jesse <jeisey91@gmail.com>
2023-08-27 12:06:55 -0400

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -9,16 +9,20 @@ current_data = pd.read_csv(url, parse_dates=['requested_datetime', 'closed_datet
 
 # Step 2: Find the most recent requested_datetime
 latest_date = current_data['requested_datetime'].max()
+latest_date = latest_date.tz_localize('UTC')
 
 # Step 3: Query the API for new or modified records
 query = f"""
-SELECT * 
+SELECT cartodb_id,objectid,service_request_id,status,status_notes,requested_datetime,updated_datetime,expected_datetime,closed_datetime,address,zipcode,media_url,lat,lon 
 FROM public_cases_fc 
 WHERE requested_datetime > '{latest_date}' OR 
       (status = 'Open' AND closed_datetime IS NOT NULL)
+      and subject = 'Graffiti Removal'
 """
 response = requests.get("https://phl.carto.com/api/v2/sql", params={'q': query})
 new_data = pd.DataFrame(response.json()['rows'])
+new_data['requested_datetime'] = pd.to_datetime(new_data['requested_datetime'])
+
 
 # Step 4: Perform upsert operation
 # Update modified records
@@ -40,12 +44,39 @@ current_data = merged_data
 
 # Append new records
 new_records = new_data[new_data['requested_datetime'] > latest_date]
-current_data = current_data.append(new_records, ignore_index=True)
+
+# Exclude records from new_data that have a cartodb_id already present in current_data
+new_records = new_data[~new_data['cartodb_id'].isin(current_data['cartodb_id'])]
+current_data = pd.concat([current_data, new_records], ignore_index=True)
+
 
 # Step 5: Recalculate time_to_close column
-current_date = datetime.now()
-current_data['time_to_close'] = (current_data['closed_datetime'].fillna(current_date) - current_data['requested_datetime']).dt.days
+current_date = pd.Timestamp.now(tz='UTC')
 
-# Save the updated dataframe (this can be pushed back to GitHub or saved locally)
+# Ensure both columns are parsed as datetime objects
+
+# Ensure 'closed_datetime' is parsed as a datetime object
+current_data['closed_datetime'] = pd.to_datetime(current_data['closed_datetime'], errors='coerce', utc=True)
+
+
+# Ensure 'requested_datetime' is parsed as a datetime object with UTC
+current_data['requested_datetime'] = pd.to_datetime(current_data['requested_datetime'], utc=True)
+
+
+
+# Convert datetime columns to proper datetime format
+current_data['requested_datetime'] = pd.to_datetime(current_data['requested_datetime'], errors='coerce', utc=True)
+current_data['closed_datetime'] = pd.to_datetime(current_data['closed_datetime'], errors='coerce', utc=True)
+
+# Handle potential NaN values in closed_datetime
+
+
+# Fill NaN values and ensure the result is a DatetimeArray
+filled_closed_datetime = pd.to_datetime(current_data['closed_datetime'].fillna(current_date))
+
+current_data['time_to_close'] = (filled_closed_datetime - current_data['requested_datetime']).dt.days
+
+
+# # Save the updated dataframe (this can be pushed back to GitHub or saved locally)
 current_data.to_csv("graffiti.csv", index=False)
 
